6faad30df57b7298eb9e15f9984d67a31c81308c
alessioraggioli <tsac047@tecnicosuperiorekennedy.it>
2016-01-28 19:03:32 +0100

    Videoteca

--- Videoteca/src/Main.java
+++ Videoteca/src/Main.java
@@ -7,20 +7,10 @@ public class Main {
 	@Test
 	public void test() {
 		
-		Dipendente d1 = new Dipendente("cristo","dio","fefefef");
-		Imprenditore i1 = new Imprenditore("buh","alemanno","sishsi","fdfd","effrfrgrgr",250);
-		Libero l1 = new Libero("buh","bah","huhioj","dsodksok",250);
-		Commercialista ferrari = new Commercialista(new Cliente[]{d1,i1,l1});
-		
 		Cassetta cas1 = new Cassetta("cristo","dio",1980,new String[]{""},3);
 		Dvd dvd1 = new Dvd("cristo","dio",1980,new String[]{""},3);
 		BluRay blu1 = new BluRay("cristo","dio",1980,new String[]{""},3);
 		Videoteca wow = new Videoteca(new Film[]{cas1,dvd1,blu1});
-		
-		System.out.println(blu1.costoNoleggio());
-		
-		//System.out.println(ferrari.incassoTot());
-		//assertEquals(875.0,ferrari.incassoTot());
 		
 		
 	}
